fb4a30b2d01841c6e0f0d94dc83500fbe25f6fb5
Nicole Sanchez <njbsanchez@gmail.com>
2021-07-24 09:43:13 -0700

    using 2 variables and a frontier to reverse linked list

---
+++ reverse.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        """
+        if head or head.next does not exist,
+        return head
+        
+        assign head as "done"
+        assign head.next as "current"
+        
+        point done.next to None
+        
+        1>none>2>3>4>none
+        d       c
+        
+        while cur exists:
+            assign current.next as "hold"
+            move current before "done" (2>1>none 3>4>none)
+            reassign current as "done"
+            reassign hold as "current"
+        
+        """
+        
+        if head is None or head.next is None:
+            return head
+        
+        done = head
+        current = head.next
+        done.next = None
+        
+        while current:
+            hold = current.next
+            current.next = done
+            
+            done = current
+            current = hold
+        
+        return done
